3c822e1720c0efcc150ab9e42d372987eb2e57af
Anna-Sara <anna-sara@selea.se>
2022-05-14 10:43:05 +0200

    Added a playHasWon function that checks if there is a winner

--- js/script.js
+++ js/script.js
@@ -138,7 +138,22 @@ const playGame = () => {
         } 
         
     }
-    
+    function playerHasWon() {
+
+        const winningsCombos = [
+            [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+        ]
+
+        for (const combo of winningsCombos) {
+            let [a,b,c] = combo;
+
+            if(cells[a].textContent && cells[a].textContent == cells[b].textContent && cells[a].textContent == cells[c].textContent){
+                console.log([a,b,c]);
+                return [a,b,c];     
+            }
+        }
+        return false;
+    }
 
     
 };
